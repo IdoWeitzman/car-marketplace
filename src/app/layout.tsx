49a69f8df602395bd3d8f081639743d18f341854
IdoWeitzman <ido.weitzman@fiverr.com>
2024-06-08 23:20:15 +0300

    install material-ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,12 @@ import {
 } from '@clerk/nextjs'
 import "./globals.css";
 
+/* material-ui Roboto font */
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
---
+++ src/app/manage-your-cars/page.tsx
@@ -0,0 +1,11 @@
+import { ManageYourCarsProps } from "./types";
+
+const ManageYourCarsPage = (props: ManageYourCarsProps) => {
+    return (
+        <div>
+        <h1>Manage your cars</h1>
+        </div>
+    );
+}
+
+export default ManageYourCarsPage;
